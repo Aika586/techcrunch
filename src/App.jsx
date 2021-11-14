bed81f02b455f59839878eb70bfcd60745e47a75
Ulan <ulanr92@gmail.com>
2021-11-14 00:54:37 -0600

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
 import React, { useEffect } from "react";
 import { hot } from "react-hot-loader/root";
-import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import News from "./components/news";
 import { getNews } from "./redux/actions";
+import { useSelector, useDispatch } from "react-redux";
 
-const App = ({ getNews, news }) => {
+const App = () => {
+  const news = useSelector((news) => state.news.news);
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    getNews();
+    dispatch(getNews());
   }, []);
 
-  console.log(news);
-  return <News news={news.news} getNews={getNews} />;
+  return <News news={news} getNews={getNews} />;
 };
 
-const mapStateToProps = function (state) {
-  return {
-    news: state.news,
-  };
-};
-
-function mapDispatchToProps(dispatch) {
-  return {
-    getNews: () => dispatch(getNews()),
-  };
-}
-
-const MainApp = connect(mapStateToProps, mapDispatchToProps)(App);
-export default hot(MainApp);
+export default hot(App);
 
 App.propTypes = {
   getNews: PropTypes.func,
